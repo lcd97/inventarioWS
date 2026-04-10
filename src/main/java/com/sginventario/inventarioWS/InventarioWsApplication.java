1c39c4af87d18839433bbb02715e793ac4519954
lcd97 <danycordero9@gmail.com>
2026-04-10 15:24:40 -0600

    Intial commit

---
+++ src/main/java/com/sginventario/inventarioWS/InventarioWsApplication.java
@@ -0,0 +1,13 @@
+package com.sginventario.inventarioWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sginventario/inventarioWS/InventarioWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sginventario.inventarioWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
