bd377f18805ec2c0786b5642e6ccef5494309250
lcd97 <danycordero9@gmail.com>
2026-04-19 10:51:03 -0600

    Configurando routes

--- inventario-ui/src/app/app.routes.server.ts
+++ inventario-ui/src/app/app.routes.server.ts
@@ -2,11 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'entradas/detalle/:id',
-    renderMode: RenderMode.Client,
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Client,
   },
 ];
