facbad3f992268e3a973577f5ebc987dbfb24159
lcd97 <danycordero9@gmail.com>
2026-04-19 10:19:05 -0600

    Configuracion de Cors

--- src/main/java/com/sginventario/inventarioWS/config/CorsConfig.java
+++ src/main/java/com/sginventario/inventarioWS/config/CorsConfig.java
@@ -11,9 +11,10 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                    "http://localhost:4200"
-                )
+                        "http://localhost:4200",
+                        "https://vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
